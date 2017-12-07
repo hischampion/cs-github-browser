ca1e509bcb92fe6a938c58a776ef65207a69a715
hischampion <scottyec@gmail.com>
2017-12-06 21:02:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World!");
+            var repos = ProcessRepositories().Result;
+
+            foreach (var repo in repos)
+            {
+                System.Diagnostics.Debug.WriteLine(repo.Name + " - " + repo.LastPush);
+            }
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            var client = new HttpClient();
+            var headers = client.DefaultRequestHeaders;
+            headers.Accept.Clear();
+            headers.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            headers.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            return serializer.ReadObject(await streamTask) as List<Repository>;
+        }
+    }
+}
---
+++ repo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WebAPIClient
+{
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name ="homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush { get; set; }
+
+        /**
+         * serializer for LastPush
+         */
+        [DataMember(Name ="pushed_at")]
+        private string JsonDate
+        {
+            get { return LastPush.ToString(); }
+
+            set
+            {
+                this.LastPush = DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
+        
+    }
+}
